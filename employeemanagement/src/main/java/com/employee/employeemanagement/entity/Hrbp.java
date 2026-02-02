8b804089d1ebcfd17243617bcbe3ccd64bf36ec6
ShriRadhey-Mishra <radheymishra12@gmail.com>
2026-02-02 16:41:37 +0530

    Created a basic entity schema for HR Business Partners

---
+++ employeemanagement/src/main/java/com/employee/employeemanagement/entity/Hrbp.java
@@ -0,0 +1,45 @@
+package com.employee.employeemanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hrbps")
+public class Hrbp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String region;
+
+    @OneToOne
+    @JoinColumn(name = "employee_id", nullable = false, unique = true)
+    private Employee employee;
+
+    public Hrbp() {}
+
+    public Hrbp(String region, Employee employee) {
+        this.region = region;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
